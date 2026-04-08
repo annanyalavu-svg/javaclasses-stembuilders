5a9e8a1d40ae29c64d9273cb6efef803efc973c7
Zorau <amazingcooldude54321@gmail.com>
2026-04-08 19:29:28 -0400

    Hello World

--- helloworldya.java
+++ helloworldya.java
@@ -1,7 +1,10 @@
 public class HelloWorld {
     public static void main (String[] args) {
-        //
+
+        static void FAHHHH() {
+            print
+        }
         System.out.println("Hello World");
     }
 }
-// hello
+// GURMAAANNNNN
